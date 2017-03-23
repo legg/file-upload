4f21958459fd046252dd26b4c893cd5fc1fd5481
Maximus <maxlegg@gmail.com>
2017-03-23 20:11:23 +0000

    Created FileSelect and FilePreview components to show how to use input type file in react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor() {
+        super()
+        this.state = {
+            source: ''
+        }
+    }
+
+    fileSelectHandler = event => {
+        const file = event.target.files[0]
+        var reader = new FileReader()
+        reader.onload = e => {
+            this.setState({source: e.target.result})
+        }
+        reader.readAsDataURL(file)
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <FileSelect onChange={this.fileSelectHandler.bind(this)}/>
+                <FilePreview source={this.state.source}/>
+            </div>
+        )
+    }
 }
 
-export default App;
+const FileSelect = ({onChange}) => {
+    return (
+        <div>
+            <input type="file" onChange={onChange}/>
+        </div>
+    )
+}
+
+const FilePreview = ({source}) => {
+    return (
+        <div>
+            <img src={source} multiple={false} alt=""/>
+        </div>
+    )
+}
+
+export default App
